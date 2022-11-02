1c6c68441c5a4a0f755daeb376e6caa0a4c84b0a
tymek.bialkowski <tymek.bialkowski@student.uj.edu.pl>
2022-11-02 04:41:08 +0100

    Zestaw 3

--- Zestaw 3/zad10.py
+++ Zestaw 3/zad10.py
@@ -3,6 +3,7 @@
 
 roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 print(roman_to_int)
+
 roman_to_int_2 = dict({'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000})
 print(roman_to_int_2)
 
